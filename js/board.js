f5096aa8fce171aee148b0fdd59da56e4402cb4a
michaeljstevens <michaeljstevens@zoho.com>
2016-09-12 00:49:24 -0700

    added some basic canvas elements

---
+++ js/board.js
@@ -0,0 +1,101 @@
+
+var canvas, stage;
+
+var mouseTarget;	// the display object currently under the mouse, or being dragged
+var dragStarted;	// indicates whether we are currently in a drag operation
+var offset;
+var update = true;
+
+function init() {
+	// examples.showDistractor();
+	// create stage and point it to the canvas:
+	canvas = document.getElementById("bodyCanvas");
+	stage = new createjs.Stage(canvas);
+
+	// enable touch interactions if supported on the current device:
+	createjs.Touch.enable(stage);
+
+	// enabled mouse over / out events
+	stage.enableMouseOver(10);
+	stage.mouseMoveOutside = true; // keep tracking the mouse even when it leaves the canvas
+
+	// load the source image:
+	var image = new Image();
+	image.src = "./img/fire.png";
+	image.onload = handleImageLoad;
+
+  const line = new createjs.Shape();
+
+  line.graphics.setStrokeStyle(1);
+  line.graphics.beginStroke("Black");
+  line.graphics.moveTo(0, 500);
+  line.graphics.lineTo(1000, 500);
+  line.graphics.endStroke();
+
+  stage.addChild(line);
+}
+
+function stop() {
+	createjs.Ticker.removeEventListener("tick", tick);
+}
+
+function handleImageLoad(event) {
+	var image = event.target;
+	var bitmap;
+	var container = new createjs.Container();
+	stage.addChild(container);
+
+	// create and populate the screen with random daisies:
+	bitmap = new createjs.Bitmap(image);
+	container.addChild(bitmap);
+	bitmap.x = canvas.width * Math.random() | 0;
+	bitmap.y = canvas.height * Math.random() | 0;
+	bitmap.regX = bitmap.image.width / 2 | 0;
+	bitmap.regY = bitmap.image.height / 2 | 0;
+	bitmap.scaleX = bitmap.scaleY = bitmap.scale = 0.1;
+	bitmap.cursor = "pointer";
+
+	// using "on" binds the listener to the scope of the currentTarget by default
+	// in this case that means it executes in the scope of the button.
+	bitmap.on("mousedown", function (evt) {
+		this.parent.addChild(this);
+		this.offset = {x: this.x - evt.stageX, y: this.y - evt.stageY};
+	});
+
+	// the pressmove event is dispatched when the mouse moves after a mousedown on the target until the mouse is released.
+	bitmap.on("pressmove", function (evt) {
+    console.log(evt.stageX);
+    console.log(evt.stageY);
+    if(this.y < 465) {
+      if(evt.stageY < 465) {
+        this.y = evt.stageY + this.offset.y;
+      }
+      this.x = evt.stageX + this.offset.x;
+    } else {
+      this.x = evt.stageX + this.offset.x;
+      this.y = evt.stageY + this.offset.y;
+    }
+		// indicate that the stage should be updated on the next tick:
+		update = true;
+	});
+
+	bitmap.on("rollover", function (evt) {
+		this.scaleX = this.scaleY = this.scale * 1.2;
+		update = true;
+	});
+
+	bitmap.on("rollout", function (evt) {
+		this.scaleX = this.scaleY = this.scale;
+		update = true;
+	});
+
+	createjs.Ticker.addEventListener("tick", tick);
+}
+
+function tick(event) {
+	// this set makes it so the stage only re-renders when an event handler indicates a change has happened.
+	if (update) {
+		update = false; // only update once
+		stage.update(event);
+	}
+}
